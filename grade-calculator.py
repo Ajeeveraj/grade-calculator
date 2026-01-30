87192d90fb12bf3b8f9d88b7212267ea811e002f
Ajeeveraj <ayushj762@deltalearns.ca>
2026-01-30 09:23:41 -0800

    defined grades
    I told it what number average turns into letter grades

---
+++ grade-calculator.py
@@ -0,0 +1,23 @@
+# welcome
+print("Welcome, please add your grades")
+
+
+# define grades
+def get_grades(avg):
+    if avg < 50:
+        return "F"
+    elif avg < 69:
+        return "D"
+    elif avg < 76:
+        return "C"
+    elif avg < 86:
+        return "B"
+    else:
+        return "A"
+    
+
+    
+    
+        
+   
+    
