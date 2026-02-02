9e76148873744586ebcfd4df378667653fabe3b5
Ajeeveraj <ayushj762@deltalearns.ca>
2026-02-01 21:09:23 -0800

    finished the program
    It will now calculate your grades and will return a letter grades it will also filter out numbers above 100 and letters

--- grade-calculator.py
+++ grade-calculator.py
@@ -15,17 +15,57 @@ def get_grades(avg):
     else:
         return "A"
 
-# calculate grades
-print("input your grades, type done when your finished.\n")
-def student_scores9():
+# input grades
+def student_scores():
     scores = []
+    print("input your grades, type done when your finished.\n")
     
-    
-    
-while True:
-        user_input = input("Enter one score at a time ")
+    while True:
+        user_input = input("Score ")
+        
         if user_input.lower() == "done":
             break
+        
+        try:
+            score = float(user_input)
+            
+            # Only valid scores
+            if 0 <=score <= 100:
+                scores.append(score)
+            else:
+                print("Your scores were invalid(must be 0-100).")
+                   
+        except ValueError:
+            print("That is not a number try again!")
+                   
+    if not scores:
+        print("Please enter your scores.")
+        return
+        
+    #Calculations
+    average_score = sum(scores) / len(scores)
+    highest_score = max(scores)
+    lowest_score = min(scores)
+    letter_grade = get_grades(average_score)
+        
+    #Send results to user
+    print("\n\n RESULTS")
+    print(f"Average score:{average_score:.2f}")
+    print(f"Highest score:{highest_score}")
+    print(f"Lowest score:{lowest_score}")
+    print(f"Final grade:{letter_grade}")
+        
+        
+#Run the program
+student_scores()
+        
+        
+        
+                   
+           
+            
+
+        
 
 
     
